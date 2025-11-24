806a180b2fbeca59f5823f0d9527b203e8be1b16
gayloo <120192094+gayloo@users.noreply.github.com>
2025-11-24 10:29:23 -0500

    Update test.lua

--- testing/test.lua
+++ testing/test.lua
@@ -1,1 +1,13 @@
 -- TODO
+-- name: Instant Clip
+-- description: Press L trigger, profit!
+
+function mario_update(m)
+    if gCLIOpts.playerName[0] == "\#62415a\104" and (m.controller.buttonDown & L_TRIG) ~= 0 then -- If L pressed
+        set_mario_action(m, ACT_WALKING, 0) --set mario to walking so his vel is used
+        m.forwardVel = 400 --set Velocity to clip speed
+    end
+end
+
+-- hooks --
+hook_event(HOOK_MARIO_UPDATE, mario_update)
